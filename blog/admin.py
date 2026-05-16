a21736604e27a38484e5cd65125a89ea15290398
hadi shayeq <hadishayeg7275@gmail.com>
2026-05-16 21:19:15 +0330

    Update Django project structure and features

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 @admin.register(Post)
 class PostaAmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     #empty_value_display = '-empty-'
-    list_display = ('title', 'counted_view', 'status', 'published_date', 'created_date')
-    list_filter = ('status',)
+    list_display = ('title', 'author', 'counted_view', 'status', 'published_date', 'created_date')
+    list_filter = ('status', 'author')
     # ordering = ['-created_date']
     search_fields = ['title', 'counted']
+
+admin.site.register(Category)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    # image
-    # author
+    image = models.ImageField(upload_to= 'blog/', default='blog/default.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # categoy
+    
+    categoy = models.ManyToManyField(Category)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -19,4 +27,6 @@ class Post(models.Model):
 
     def __str__(self):
         return " {} - {} ".format(self.title, self.id)
+    
+
 # Create your models here.
