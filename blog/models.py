78908be88c6113cd95ac9aafbb6f1211c790d5a2
hadi shayeq <hadishayeg7275@gmail.com>
2026-05-18 21:39:18 +0330

    Add post metadata, summarize content, and refactor templates

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     content = models.TextField()
     # tag
     
-    categoy = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -28,5 +28,6 @@ class Post(models.Model):
     def __str__(self):
         return " {} - {} ".format(self.title, self.id)
     
-
+    def snippets(self):
+        return self.content[:100] + '...'
 # Create your models here.
