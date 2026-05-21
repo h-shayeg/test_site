12bb4d776a53acbb78179bb96a953c766adad846
hadi shayeq <hadishayeg7275@gmail.com>
2026-05-21 21:45:46 +0330

    Implement tagging system and update related views and templates

--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,7 @@ def blog_single(request, pid):
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
-def test(request, pid):
-    #post =  Post.objects.get(id = pid)
-    Posts = Post.objects.filter(status = 1)
-    post = get_object_or_404(Post, pk = pid)
-    context = {'post': post}
-    return render(request, 'test.html', context)
+def test(request):
+    return render(request,'test.html')
 
 # Create your views here.
