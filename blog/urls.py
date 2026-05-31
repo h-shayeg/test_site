05ac34fcf34156fa7b8f1f6b106aba119863a6ce
hadi shayeq <hadishayeg7275@gmail.com>
2026-05-31 22:09:51 +0330

    Implement post search and filtering functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import blog_view, blog_single, test, blog_category
+from blog.views import blog_view, blog_single, test, blog_category, blog_search
 
 app_name = 'blog'
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('',blog_view, name = 'index'),
     path('<int:pid>', blog_single, name = 'single'),
     path('category/<str:cat_name>', blog_view, name='category'),
+    path('author/<str:author_username>',blog_view, name='author'),
+    path('search/', blog_search, name = 'search'),
     path('test',test,name='test'),
     #path('post-<int:pid>', test, name = 'test'),
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog_view(request, cat_name=None):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status = 1)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') !=None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') !=None:
+        posts = posts.filter(author__username = kwargs['author_username'])
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -20,5 +22,14 @@ def blog_category(request, cat_name):
     posts = Post.objects.filter(status = 1)
     posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains = s)
+    
+    context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 # Create your views here.
