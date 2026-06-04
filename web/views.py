f07885c362095d4313af2c6bf507ddae9fc1edb9
hadi shayeq <hadishayeg7275@gmail.com>
2026-06-04 17:21:26 +0330

    fix(contact): handle GET requests and add success/error messages

--- web/views.py
+++ web/views.py
@@ -1,18 +1,24 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from django.http import HttpResponse, JsonResponse
 from web.models import contact
 from web.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
 
 def about_view(request):
     return render(request, 'website/about.html')
 
 def content_view(request):
+    form = ContactForm()
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-    else:
-        form = ContactForm()
+            messages.add_message(request, messages.SUCCESS, 'Your message has been sent successfully!')
+            form = ContactForm()
+        else:
+            messages.add_message(request, messages.ERROR, 'There was an error sending your message. Please try again.')
+       
     return render(request, 'website/contact.html', {'form': form})
 
 def newsletter_view(request):
