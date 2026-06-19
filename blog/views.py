c143dcb2bf78c79c2419194971e992b6309ee03e
Hadi <hadishayeg7275@gmail.com>
2026-06-19 22:47:14 +0330

    Add comment form support to custom commenting system

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.forms import CommentForm
+from django.contrib import messages
 
 def blog_view(request, **kwargs):
     posts = Post.objects.filter(status = 1)
@@ -26,10 +28,18 @@ def blog_view(request, **kwargs):
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'your comment submited successfuly')
+        else:
+            messages.add_message(request, messages.ERROR, 'your comment didnt submited')
     posts = Post.objects.filter(status = 1)
     post = get_object_or_404(posts, pk = pid)
     comments = Comment.objects.filter(post = post.id, approved=True)
-    context = {'post': post, 'comments': comments}
+    form = CommentForm()
+    context = {'post': post, 'comments': comments, 'form': form}
     return render(request, 'blog/blog-single.html', context)
 
 def test(request):
