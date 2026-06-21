35581dfc82565be6b2b99c5bc1bff347c48b9508
Hadi <hadishayeg7275@gmail.com>
2026-06-21 20:43:06 +0330

    Add RSS feed support for blog content syndication

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog.views import blog_view, blog_single, test, blog_category, blog_search
+from blog.feeds import LatestEntriesFeed
 
 app_name = 'blog'
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('author/<str:author_username>',blog_view, name='author'),
     path('search/', blog_search, name = 'search'),
     path('test',test,name='test'),
+    path('rss/feed/', LatestEntriesFeed()),
     #path('post-<int:pid>', test, name = 'test'),
 
 ]
